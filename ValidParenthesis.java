d49ac095d5c6462ee47f0fd46abcd8ab8566a739
kunwaraman <amankunwar8472@gmail.com>
2025-02-21 00:19:39 +0530

    day21

---
+++ ValidParenthesis.java
@@ -0,0 +1,33 @@
+import java.util.*;
+/*
+time complexity: O(n)
+space complexity: O(n)
+ */
+public class ValidParenthesis {
+    int n = s.length();
+    public static boolean valid(String s){
+    Stack<Character> stack = new Stack<>();
+        for(char ch:s.toCharArray()){
+        if(ch=='('||ch=='{'|| ch=='['){
+            stack.push(ch);
+        }else{
+            if(ch==')'){
+                if(stack.isEmpty() || stack.pop()!='('){
+                    return false;
+                }
+            }
+            if(ch=='}'){
+                if(stack.isEmpty() || stack.pop()!='{'){
+                    return false;
+                }
+            }
+            if(ch==']'){
+                if(stack.isEmpty() || stack.pop()!='['){
+                    return false;
+                }
+            }
+        }
+      }
+        return stack.isEmpty();
+    }
+}
